fb3a976115eb455ed98b8f87885435e7512a1ef2
Suganthan1986 <137049216+Suganthan1986@users.noreply.github.com>
2023-06-19 17:06:40 +0530

    Add files via upload
    Day1

---
+++ day1/Iphone.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Iphone {
+
+	public void makeCall() 
+	{
+		System.out.println(" make call to this number : 9884512345");
+	}
+	  public void model()  
+	  {
+		System.out.println("model is: Infinix ");
+	 }
+	
+
+}
---
+++ day1/SmartPhone.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class SmartPhone {
+
+	
+	public static void main(String[] args) {
+		
+		Iphone i = new Iphone();
+		
+		i.makeCall();
+		i.model();
+		
+	}
+}
